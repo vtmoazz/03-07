70fb17057d9d2b2c988cf1083d93106258bf7f3a
Van Thanh <136183859+vtmoazz@users.noreply.github.com>
2024-07-03 12:10:16 +0700

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int count_vowels(char *s) {
+    int count = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = tolower(s[i]); 
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    char s[101];
+
+    printf("Enter s: ");
+    fgets(s, 101, stdin);
+
+    int vowel_count = count_vowels(s);
+
+    printf("\nOUTPUT\n%d\n", vowel_count);
+
+    return 0;
+}
