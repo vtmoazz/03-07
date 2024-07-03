ac168c2120e4420ecd8f60f5893809138895ae26
Van Thanh <136183859+vtmoazz@users.noreply.github.com>
2024-07-03 20:24:43 +0700

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -6,7 +6,7 @@ int count_vowels(char *s) {
     int count = 0;
     for (int i = 0; s[i] != '\0'; i++) {
         char c = tolower(s[i]); 
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ) {
             count++;
         }
     }
