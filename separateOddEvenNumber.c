a3a1369e5801e35b1f37b903b654d025514c5785
Van Thanh <136183859+vtmoazz@users.noreply.github.com>
2024-07-03 12:27:15 +0700

    Add files via upload

---
+++ separateOddEvenNumber.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void separateNum(int arr[], int n) {
+    int i;
+    printf("\nOUTPUT\n");
+    printf("Odd numbers: ");
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+    printf("\n");
+    printf("Even numbers: ");
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    separateNum(arr, n);
+
+    return 0;
+}
